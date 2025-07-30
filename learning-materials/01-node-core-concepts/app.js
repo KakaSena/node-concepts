182b1dde1ff21a6dbe70c2e7c50ddb900e600eb1
kaka <kakaxsena@gmail.com>
2025-07-30 16:49:31 -0300

    redirecting requests

--- learning-materials/01-node-core-concepts/app.js
+++ learning-materials/01-node-core-concepts/app.js
@@ -1,8 +1,28 @@
 const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const filePath = path.join(__dirname, 'message.txt');
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
-  // process.exit();
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message </title> </head>');
+    res.write(
+      '<body><form action="/message" method="POST"> <input type="text" name="message"><button type="submit">Send</button> </form> </body>'
+    );
+    res.write('</html>');
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync(filePath, 'DUMMY');
+    res.writeHead(302, { Location: '/' });
+    return res.end();
+  }
+
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title> My first Page </title> </head>');
